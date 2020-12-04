590d23f7838452f8cbdbec77f3f8faac1458a29b
Nathaniel Daniel <nathanieldaniel@nevada.unr.edu>
2020-12-03 18:08:32 -0800

    Add button support

--- CPE301_Project.ino
+++ CPE301_Project.ino
@@ -16,61 +16,64 @@ void setIdleOutputs();
 void setRunningOutputs();
 void setErrorOutputs();
 
+const unsigned char BUTTON_PIN = 20;
 
 int waterthreshold = 3;
 int temphighthreshold = 143;
 int templowthreshold = 140;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-int waterlevel; //holds latest water level
-int temperature;  //holds latest temperature
+int waterlevel;  //holds latest water level
+int temperature; //holds latest temperature
+
+volatile bool buttonPressed = false;
 
 class StateInterface
 {
-  protected:
-    SwampCooler *sc;
-  public:
-    virtual void disable_enable();
-    virtual void checkWater();
-    virtual void checkTemp();
+protected:
+  SwampCooler *sc;
+
+public:
+  virtual void disable_enable();
+  virtual void checkWater();
+  virtual void checkTemp();
 };
 
 class DisabledState : public StateInterface
 {
-  public:
-    DisabledState(SwampCooler *s);
-    void disable_enable();
-    void checkWater();
-    void checkTemp();
+public:
+  DisabledState(SwampCooler *s);
+  void disable_enable();
+  void checkWater();
+  void checkTemp();
 };
 
 class IdleState : public StateInterface
 {
-  public:
-    IdleState(SwampCooler *s);
-    void disable_enable();
-    void checkWater();
-    void checkTemp();
+public:
+  IdleState(SwampCooler *s);
+  void disable_enable();
+  void checkWater();
+  void checkTemp();
 };
 
 class RunningState : public StateInterface
 {
-  public:
-    RunningState(SwampCooler *s);
-    void disable_enable();
-    void checkWater();
-    void checkTemp();
+public:
+  RunningState(SwampCooler *s);
+  void disable_enable();
+  void checkWater();
+  void checkTemp();
 };
 
 class ErrorState : public StateInterface
 {
-  public:
-    ErrorState(SwampCooler *s);
-    void disable_enable();
-    void checkWater();
-    void checkTemp();
+public:
+  ErrorState(SwampCooler *s);
+  void disable_enable();
+  void checkWater();
+  void checkTemp();
 };
-
 
 class SwampCooler
 {
@@ -79,18 +82,17 @@ class SwampCooler
   IdleState idle;
   RunningState running;
   ErrorState error;
-  
-  public:
-    int state;
-  
-    SwampCooler();
-    void update();
-    void setDisabled();
-    void setIdle();
-    void setRunning();
-    void setError();
-};
 
+public:
+  int state;
+
+  SwampCooler();
+  void update();
+  void setDisabled();
+  void setIdle();
+  void setRunning();
+  void setError();
+};
 
 //********************DisabledState Methods********************
 DisabledState::DisabledState(SwampCooler *s)
@@ -98,7 +100,7 @@ DisabledState::DisabledState(SwampCooler *s)
   sc = s;
 }
 
-void  DisabledState::disable_enable()
+void DisabledState::disable_enable()
 {
   sc->state = 1;
   setIdleOutputs();
@@ -117,7 +119,7 @@ IdleState::IdleState(SwampCooler *s)
   sc = s;
 }
 
-void  IdleState::disable_enable()
+void IdleState::disable_enable()
 {
   setDisabledOutputs();
   sc->state = 0;
@@ -125,7 +127,7 @@ void  IdleState::disable_enable()
 }
 void IdleState::checkWater()
 {
-  if(getWaterLevel()<waterthreshold)
+  if (getWaterLevel() < waterthreshold)
   {
     setErrorOutputs();
     sc->state = 3;
@@ -134,7 +136,7 @@ void IdleState::checkWater()
 }
 void IdleState::checkTemp()
 {
-  if(getTemperature()>temphighthreshold)
+  if (getTemperature() > temphighthreshold)
   {
     setRunningOutputs();
     sc->state = 2;
@@ -148,7 +150,7 @@ RunningState::RunningState(SwampCooler *s)
   sc = s;
 }
 
-void  RunningState::disable_enable()
+void RunningState::disable_enable()
 {
   setDisabledOutputs();
   sc->state = 0;
@@ -156,7 +158,7 @@ void  RunningState::disable_enable()
 }
 void RunningState::checkWater()
 {
-  if(getWaterLevel()<waterthreshold)
+  if (getWaterLevel() < waterthreshold)
   {
     setErrorOutputs();
     sc->state = 3;
@@ -165,7 +167,7 @@ void RunningState::checkWater()
 }
 void RunningState::checkTemp()
 {
-  if(getTemperature()<templowthreshold)
+  if (getTemperature() < templowthreshold)
   {
     setIdleOutputs();
     sc->state = 1;
@@ -179,7 +181,7 @@ ErrorState::ErrorState(SwampCooler *s)
   sc = s;
 }
 
-void  ErrorState::disable_enable()
+void ErrorState::disable_enable()
 {
   setDisabledOutputs();
   sc->state = 0;
@@ -187,7 +189,7 @@ void  ErrorState::disable_enable()
 }
 void ErrorState::checkWater()
 {
-  if(getWaterLevel()>waterthreshold)
+  if (getWaterLevel() > waterthreshold)
   {
     sc->state = 1;
     setIdleOutputs();
@@ -198,10 +200,9 @@ void ErrorState::checkTemp()
 {
 }
 
-
 //********************SwampCooler Methods********************
 
-SwampCooler::SwampCooler() : disabled{this},idle{this},running{this},error{this}
+SwampCooler::SwampCooler() : disabled{this}, idle{this}, running{this}, error{this}
 {
   state = 1;
   currentstate = &idle;
@@ -209,11 +210,17 @@ SwampCooler::SwampCooler() : disabled{this},idle{this},running{this},error{this}
 
 void SwampCooler::update()
 {
+  if (buttonPressed)
+  {
+    currentstate->disable_enable();
+    buttonPressed = false;
+  }
+
   //Serial.println(state);
   currentstate->checkWater();
   currentstate->checkTemp();
 }
-  
+
 void SwampCooler::setDisabled()
 {
   currentstate = &disabled;
@@ -230,7 +237,6 @@ void SwampCooler::setError()
 {
   currentstate = &error;
 }
-
 
 //***************OUTPUT FUNCTIONS****************
 void disableAll()
@@ -254,14 +260,14 @@ void setIdleOutputs()
   disableAll();
   Serial.println("Green on");
   Serial.print("Changed state to idle at: ");
-  Serial.println(millis()/1000.0f);
+  Serial.println(millis() / 1000.0f);
 }
 
 void setRunningOutputs()
 {
   disableAll();
   Serial.print("Changed state to running at: ");
-  Serial.println(millis()/1000.0f);
+  Serial.println(millis() / 1000.0f);
   Serial.println("Blue on");
 }
 
@@ -271,7 +277,6 @@ void setErrorOutputs()
   Serial.println("Red on");
 }
 
-
 int getWaterLevel()
 {
   waterlevel = analogRead(A0);
@@ -280,15 +285,13 @@ int getWaterLevel()
 
 int getTemperature()
 {
-   temperature = analogRead(A1);
-   return temperature;
+  temperature = analogRead(A1);
+  return temperature;
 }
 
 //************MAIN***************
 
 SwampCooler swampcooler;
-bool statechange = 1;  //check if state has changed in last iteration
-int staterequest = 0;
 //int state = 0;
 //states:
 //0- disabled
@@ -296,24 +299,33 @@ int staterequest = 0;
 //2-running
 //3-error
 
-void setup() {
-  Serial.begin(9600);
-  lcd.begin(16,2);
+void processButtonPressISR()
+{
+  buttonPressed = true;
 }
 
-void loop() {
+void setup()
+{
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), processButtonPressISR, FALLING);
+
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+}
+
+void loop()
+{
   delay(200);
-  //lcd.print(getWaterLevel());
+
+  // lcd.print(getWaterLevel());
   swampcooler.update();
-  
+
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Water Level: ");
   lcd.print(waterlevel);
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Temperature: ");
   lcd.print(temperature);
-  
-
 }
